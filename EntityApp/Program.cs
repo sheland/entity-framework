4e37792d78c3d9bf877939387634c1ba0b1986b2
Shelan <shelandebesai@gmail.com>
2020-05-05 21:35:19 -0700

    first commit

---
+++ EntityApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using static EntityApp.Program.Album;
+
+namespace EntityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MusicContext())
+            {
+                var albums = context.Albums;
+            }
+        }
+
+        public class Album
+        {
+            public int AlbumId { get; set; }
+            public string Title { get; set; }
+            public decimal Price { get; set; }
+
+            public class MusicContext : DbContext
+            {
+                public DbSet<Album> Albums { get; set; }
+            }
+        }
+    }
+}
